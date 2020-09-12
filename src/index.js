3df3dd22e6548f2fe9ea97ebd1790c1cbe89d281
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-11 21:15:18 -0500

    add removeAll option

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 'use strict';
 
+// remove for file that only have 'not strict'
+function removeSelective(path, t) {
+    if (path.node.value.value === 'use strict') {
+        const siblings = path.container // get all the sibilings (code in the same level) of 'use strict'
+
+        for (const sibling of siblings) {
+            if (t.isDirective(sibling)) { // if the sibiling is a directive
+                if (sibling.value.value === 'not strict') { // check if its 'not strict'
+                    path.remove(); // remove 'use strict'
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// remove for all files
+function removeAll(path) {
+    if (path.node.value.value === 'use strict') {
+        path.node.value.value = 'not strict' // replace 'use strict' with 'not strict'
+    }
+}
+
+
 module.exports = function ({ types: t }) {
     return {
         name: 'transform-not-strict',
         visitor: {
-            Directive(path) {
-                if (path.node.value.value === 'use strict') {
-                    const siblings = path.container // get all the sibilings (code in the same level) of 'use strict'
-
-                    for (const sibling of siblings) {
-                        if (t.isDirective(sibling)) { // if the sibiling is a directive
-                            if (sibling.value.value === 'not strict') { // check if its 'not strict'
-                                path.remove(); // remove 'use strict'
-                                break;
-                            }
-                        }
-                    }
+            Directive(path, state) {
+                if (state.opts.removeAll) {
+                    removeAll(path) // remove for all files
+                } else {
+                    removeSelective(path, t) // remove for file that only have 'not strict'
                 }
             },
         },
